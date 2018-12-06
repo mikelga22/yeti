b4a7debc840ae88b3ac6aa0df6a4293446e34b69
mikel <mikel.garralaga@estudiant.upc.edu>
2018-12-06 12:17:41 +0100

    Add nvt class

--- core/openvas/openvas.py
+++ core/openvas/openvas.py
@@ -18,6 +18,7 @@ class Result(EmbeddedDocument):
     severity=DecimalField()
     qod=IntField()
     description=StringField()
+    nvt=ReferenceField('Nvt')
 
 
     def create(self, res):
@@ -27,18 +28,58 @@ class Result(EmbeddedDocument):
         self.threat=res.find('threat').text
         self.severity=res.find('severity').text
         self.qod=res.find('qod').find('value').text
-        self.description=res.find('description').text.split('\n') if res.find('description').text else None
-        #self.description=" ".join(re.split("\s+", res.find('description').text, flags=re.UNICODE))
-        #self.nvt=Nvt(res.find('nvt'))
+        self.description=res.find('description').text if res.find('description') is not None else None
+        self.description=self.description.split('\n') if self.description is not None else None
+        self.nvt=Nvt().create(res.find('nvt')).id
 
         return self
+
+class Nvt(Document):
+    # oid=StringField(verbose_name="OID", unique=True
+    oid = StringField(verbose_name="OID")
+    name=StringField(verbose_name="Name")
+    references=ListField(verbose_name="References")
+    info=ListField(verbose_name="Information")
+    certs=ListField(verbose_name="Certs")
+    cve=StringField(verbose_name="CVE")
+
+
+    def create(self,nvt):
+        self.oid=nvt.attrib.values()[0]
+        self.name = nvt.find('name').text
+        self.references=[]
+        self.references = nvt.find('xref').text.split('\n')
+        #self.info=[]
+        self.info=nvt.find('tags').text.split('|')
+        self.cve = nvt.find('cve').text.replace(' ', '')
+        #self.certs=[]
+        self.certs=self.get_certs(nvt.find('cert'))
+
+        obj=self.save()
+        # try:
+        #     obj=Nvt.objects.get(oid=self.oid)
+        # except DoesNotExist:
+        #     obj=self.save()
+
+        return obj
+
+    def get_certs(self, certs):
+        list=[]
+        for cert in certs:
+            list.append(cert.attrib.values()[0])
+        return list
+
+    def get_tags(self,tags):
+        list=[]=tags.split('|')
+        return list
+
 
 class Openvas (Investigation):
     oid=StringField(verbose_name="OID")
     name=StringField(verbose_name="Name")
     report_date=DateTimeField(verbose_name="Date of report")
-    hosts=StringField(verbose_name="Host")
-    ports=StringField(verbose_name="Ports")
+    hosts=ListField(verbose_name="Host")
+    ports=ListField(verbose_name="Ports")
     results_count=IntField(verbose_name="Rresults count")
     severity=DecimalField(verbose_name="Severity")
     results=ListField(EmbeddedDocumentField(Result), verbose_name="Results")
@@ -60,13 +101,11 @@ class Openvas (Investigation):
 
         self.report_date = datetime.strptime(file.getroot().find('creation_time').text,'%Y-%m-%dT%H:%M:%SZ')
 
-        self.hosts = []
         for h in report.findall('host'):
             self.hosts.append(h.find('ip').text)
         # self.host = report.find('host').find('ip').text
         #self.hosts = self.hosts[:-2].replace('\n', '')
 
-        self.ports = []
         for port in report.find('ports').findall('port'):
             self.ports.append(port.text)
         #.ports = self.ports[:-2].replace('\n', '')#.replace(' ', '')
@@ -78,3 +117,6 @@ class Openvas (Investigation):
             self.results.append(result)
 
         return self;
+
+    def get_hosts(self,hosts):
+        pass
